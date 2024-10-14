e500afaa4216e91750f46d1c626afb8fc1fc4763
Lucas Araujo <lsa77793@gmail.com>
2024-10-13 22:23:49 -0300

    Metodo Parse

---
+++ Manejo de errores/MetodoParse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class MParse
+{
+	public Class1()
+	{
+		static void Main (string[] args)
+		{
+			string NumeroEnCadena = "10";
+			int valor = int.Parse(NumeroEnCadena);
+
+			Console.WriteLine(valor);
+
+			Console.ReadLine();
+		}
+	}
+}
